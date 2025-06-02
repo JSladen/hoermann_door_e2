549f29ce206a77894a5bf5c5a833092c7adef6ff
JSladen <104736719+JSladen@users.noreply.github.com>
2025-06-03 00:46:03 +0100

    Update __init__.py

--- esphome/components/uapbridge/button/__init__.py
+++ esphome/components/uapbridge/button/__init__.py
@@ -8,9 +8,11 @@ DEPENDENCIES = ["uapbridge"]
 
 UAPBridgeButtonVent = uapbridge_ns.class_("UAPBridgeButtonVent", button.Button, cg.Component)
 UAPBridgeButtonImpulse = uapbridge_ns.class_("UAPBridgeButtonImpulse", button.Button, cg.Component)
+UAPBridgeButtonLight = uapbridge_ns.class_("UAPBridgeButtonLight", button.Button, cg.Component)
 
 CONF_BUTTON_VENT = "vent_button"
 CONF_BUTTON_IMPULSE = "impulse_button"
+CONF_BUTTON_LIGHT = "light_button"
 
 CONFIG_SCHEMA = cv.Schema(
     {
@@ -20,6 +22,9 @@ CONFIG_SCHEMA = cv.Schema(
         ),
         cv.Optional(CONF_BUTTON_IMPULSE): button.button_schema(
             UAPBridgeButtonImpulse, icon="mdi:arrow-up-down"
+        ),
+        cv.Optional(CONF_BUTTON_LIGHT): button.button_schema(
+            UAPBridgeButtonLight, icon="mdi:light"
         ),
     }
 ).extend(cv.COMPONENT_SCHEMA)
@@ -36,3 +41,8 @@ async def to_code(config):
         impulse_button = await button.new_button(conf)
         await cg.register_component(impulse_button, conf)
         cg.add(impulse_button.set_uapbridge_parent(parent))
+
+    if conf := config.get(CONF_BUTTON_LIGHT):
+        light_button = await button.new_button(conf)
+        await cg.register_component(light_button, conf)
+        cg.add(light_button.set_uapbridge_parent(parent))
